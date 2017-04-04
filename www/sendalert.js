e18f6aa5db945063e4252df5b014246d223ca659
suley22 <quaringaston@gmail.com>
2017-04-04 14:19:39 -0300

    Update sendalert.js

--- www/sendalert.js
+++ www/sendalert.js
@@ -1,7 +1,7 @@
 var SendAlert = function() {};
 
 SendAlert.prototype.send = function(successCallback, errorCallback,msj) {
-	cordova.exec(successCallback, errorCallback, "SendAlert","send", ["msj":msj]);
+	cordova.exec(successCallback, errorCallback, "SendAlert","send", [{"msj":msj}]);
 };
 
 var helloWorld = new SendAlert();
